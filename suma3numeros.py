8fa878706800634c326997bd2edda53b92671e34
alondragomez34-dev <alondra.gomez34@unach.mx>
2026-02-05 23:26:51 -0600

    suma de 3 números

---
+++ suma3numeros.py
@@ -0,0 +1,14 @@
+def sumatoria_tres_digitos():
+    num1 = int(input("Ingresa el primer numero: "))
+    num2 = int(input("Ingresa el segundo numero: "))
+    num3 = int(input("Ingresa el tercer numero: "))
+
+    suma = num1 + num2 + num3
+    print("La sumatoria es:", suma)
+
+
+sumatoria_tres_digitos()
+
+
+
+
---
+++ sumade3numeros.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, num3, suma;
+
+    cout << "Ingresa el primer numero: ";
+    cin >> num1;
+
+    cout << "Ingresa el segundo numero: ";
+    cin >> num2;
+
+    cout << "Ingresa el tercer numero: ";
+    cin >> num3;
+
+    suma = num1 + num2 + num3;
+
+    cout << "La sumatoria es: " << suma << endl;
+
+    return 0;
+}
